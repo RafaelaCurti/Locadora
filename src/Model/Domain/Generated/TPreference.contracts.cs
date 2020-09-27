f5441662aae93e1204a7526c71416d0594769718
Rafaela Curti de Paula <rcurtidepaula@gmail.com>
2020-09-27 18:07:26 -0300

    simple refresh

--- src/Model/Domain/Generated/TPreference.contracts.cs
+++ src/Model/Domain/Generated/TPreference.contracts.cs
@@ -2,14 +2,16 @@ using Simple.Entities;
 using Locadora.Domain;
 using Simple.Services;
 using Locadora.Services;
+using System;
 
 namespace Locadora.Domain
 {
     public partial class TPreference
     {
-        public static void SavePreferences(TClient model)
+        public static void SavePreferences(TClient model) 
         {
-            Service.SavePreferences(model);
-        }
+			Service.SavePreferences(model);
+		}
+
     }
 }
--- src/Model/Services/ITPreferenceService.cs
+++ src/Model/Services/ITPreferenceService.cs
@@ -2,6 +2,7 @@ using Simple.Entities;
 using Locadora.Domain;
 using Simple.Services;
 using Locadora.Services;
+using System;
 
 namespace Locadora.Services
 {
